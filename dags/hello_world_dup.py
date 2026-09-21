f06fbc9358eea295e875772f3a34dc5098eeb546
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2026-09-21 16:00:36 -0600

    Update hello_world_dup.py

--- dags/hello_world_dup.py
+++ dags/hello_world_dup.py
@@ -1,5 +1,5 @@
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
-with DAG(dag_id="hello_world", schedule=None, tags=["training", "git-bundle"]) as dag:
+with DAG(dag_id="hello_world_dup", schedule=None, tags=["training", "git-bundle"]) as dag:
     BashOperator(task_id="say_hello", bash_command="echo hello from ecs task-runner, loaded via GitDagBundle")
